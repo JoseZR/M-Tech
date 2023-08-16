dde7f38e285240ab5dda5578361aa3988f0aa6cf
JoseZR <josezerrod@hotmail.com>
2023-08-16 13:31:31 -0600

    se desabilita formulario de registro

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { StrategicAllies } from './components/StrategicAllies/StrategicAllies'
 import './App.css'
 import { Category } from './components/Category/Category'
 import { Dates } from './components/Dates/Dates'
-import { Record } from './components/Record/Record'
+// import { Record } from './components/Record/Record'
 
 function App () {
   return (
@@ -19,7 +19,7 @@ function App () {
         <Dates />
       </div>
       <div className='background'>
-        <Record />
+        {/* <Record /> */}
       </div>
       <StrategicAllies />
       <Footer />
