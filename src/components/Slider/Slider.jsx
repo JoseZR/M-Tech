e7588ba184c4cccb97f4327276d73e524f19d9fc
JoseZR <josezerrod@hotmail.com>
2023-10-31 03:14:51 -0600

    se agrega ruta de poster de video

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+// import { useState, useEffect, useRef } from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import banner1 from '../../../public/img/banner_01.webp'
 import banner2 from '../../../public/img/banner_02.webp'
@@ -10,7 +10,7 @@ export function Slider () {
     <div className='position-relative'>
       <Carousel fade interval={null} controls={false}>
         <Carousel.Item>
-          <video className='w-100' controls src='/MTech/img/final_mtech_2023.mp4' poster='/img/posterVideoMTech2023.webp' loading='lazy' autoPlay muted playsInline />
+          <video className='w-100' controls src='/MTech/img/final_mtech_2023.mp4' poster='/MTech/img/posterVideoMTech2023.webp' loading='lazy' autoPlay muted playsInline />
         </Carousel.Item>
         <Carousel.Item>
           <img
