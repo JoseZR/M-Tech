c335d6eaeac2b49b168a92fa4ea27f416d445d50
JoseZR <josezerrod@hotmail.com>
2023-11-09 16:38:38 -0600

    se ajusta fecha en seccion de about us

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -15,7 +15,7 @@ export function AboutUs () {
       <Container>
         <Row className='pt-5 mt-5 mb-5'>
           <Col xs={12} md={12} lg={12} className='text-center'>
-            <p className='title-mtech'>M-Tech 2023</p>
+            <p className='title-mtech'>M-Tech 2024</p>
           </Col>
         </Row>
         <Row>
@@ -44,7 +44,7 @@ export function AboutUs () {
           <Col xs={12} md={6} lg={6} className='cont-text-bot text-center py-4 px-5' data-aos='fade-zoom-in' data-aos-easing='ease-in-back' data-aos-delay='200' data-aos-offset='0' data-aos-duration='1000'>
             <div>
               <p>Entonces</p> <br />
-              <h2 className='title-bot'> ¡M-Tech 2023 es para ti!</h2><br />
+              <h2 className='title-bot'> ¡M-Tech 2024 es para ti!</h2><br />
               <p>Desafía al futuro y demuestra tu talento en la segunda edición de la competencia de robótica que el Gobierno del Estado de Guanajuato e Italian German Exhibition Company Mexico organizan para ti</p>
             </div>
           </Col>
@@ -54,7 +54,7 @@ export function AboutUs () {
             xs={12} md={6} lg={6} className='card-1 cont-text-bot text-center my-5 py-5 px-5' data-aos='fade-zoom-in' data-aos-easing='ease-in-back' data-aos-delay='200' data-aos-offset='0' data-aos-duration='1000'
           >
             <p>
-              Las categorías de M-Tech 2023 te permitirán sobrepasar los límites de tu imaginación, poner a prueba tus habilidades en robótica, crear innovaciones y potenciar tus habilidades blandas
+              Las categorías de M-Tech 2024 te permitirán sobrepasar los límites de tu imaginación, poner a prueba tus habilidades en robótica, crear innovaciones y potenciar tus habilidades blandas
             </p>
           </Col>
           <Col xs={12} md={6} lg={6} className='card-2 d-flex justify-content-center py-5 px-5'>
