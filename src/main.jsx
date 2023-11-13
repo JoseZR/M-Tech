8ad53a41d215279b2dc8b64b12498071b082c898
JoseZR <josezerrod@hotmail.com>
2023-11-13 10:54:24 -0600

    actualizacion en ruta de pagina web

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename=''>
+  <BrowserRouter basename='MTech'>
     <App />
   </BrowserRouter>
 )
