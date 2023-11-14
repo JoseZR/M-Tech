8607e7a3fc3e1b8e1d3f8b1033da27a782cceca2
JoseZR <josezerrod@hotmail.com>
2023-11-14 08:41:03 -0600

    se agrega efecto hover a boton

--- src/components/Gallery/GalleryPage.jsx
+++ src/components/Gallery/GalleryPage.jsx
@@ -10,7 +10,7 @@ export function GalleryPage () {
       <center><h1 className='title-gallery'>LO MEJOR DE M-TECH 2023</h1></center>
       <Container className='galleryPage mt-5'>
         <GalleryYear gallery={BestMoments} />
-        <center><br /><br />
+        <center><br />
           <Link to='/gallery2023' className='text-decoration-none'>
             <button class='btn-best-moments my-3'><span class='btn-best-moments-content'>VER GALERIA</span></button>
           </Link>
